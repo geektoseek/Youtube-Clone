330d0b8bca78ec567d40ee30dd6f32208871cfce
NAEEM <142893108+geektoseek@users.noreply.github.com>
2026-03-03 16:31:27 +0500

    Working on Api to show category content and feed content

--- src/Components/Feed.jsx
+++ src/Components/Feed.jsx
@@ -1,132 +1,46 @@
-import React from 'react'
-import thumbnail1 from '../assets/thumbnail1.png'
-import thumbnail2 from '../assets/thumbnail2.png'
-import thumbnail3 from '../assets/thumbnail3.png'
-import thumbnail4 from '../assets/thumbnail4.png'
-import thumbnail5 from '../assets/thumbnail5.png'
-import thumbnail6 from '../assets/thumbnail6.png'
-import thumbnail7 from '../assets/thumbnail7.png'
-import thumbnail8 from '../assets/thumbnail8.png'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
+import { myAPI, value_convertor } from '../data'
+import moment from 'moment'
 
+const Feed = ({ cateory }) => {
 
+    const [data, setData] = useState([]);
 
-const Feed = () => {
+    const fetchData = async () => {
+        const videoList = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&videoCategoryId=${cateory}&key=${myAPI}`;
+        await fetch(videoList)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data)
+                if (data.items) {
+                    setData(data.items)
+                }
+            })
+    }
+
+    useEffect(() => {
+        fetchData();
+    }, [cateory])
 
     return (
         <>
-            <div class="feed grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-4 gap-y-7 mt-4">
-
-                <Link to={`video/20/4521`} className="card">
-                    <img className='w-full rounded-sm' src={thumbnail1} alt="" />
-                    <h2 className='font-semibold text-[16px] text-black mx-0 my-1.5'
-                    >How to earn Money from Youtube with in month just 30 Days.</h2>
-                    <h3 className='text-sm font-semibold text-[#555] mx-0 my-2'
-                    >FraudTv</h3>
-                    <p className='text-sm' >15m views  &bull; 2 day ago</p>
-                </Link>
-
-                <div className="card">
-                    <img className='w-full rounded-sm' src={thumbnail2} alt="" />
-                    <h2 className='font-semibold text-[16px] text-black mx-0 my-1.5'
-                    >How to earn Money from Youtube with in month just 30 Days.</h2>
-                    <h3 className='text-sm font-semibold text-[#555] mx-0 my-2'
-                    >FraudTv</h3>
-                    <p className='text-sm' >15m views  &bull; 2 day ago</p>
-                </div>
-
-                <div className="card">
-                    <img className='w-full rounded-sm' src={thumbnail3} alt="" />
-                    <h2 className='font-semibold text-[16px] text-black mx-0 my-1.5'
-                    >How to earn Money from Youtube with in month just 30 Days.</h2>
-                    <h3 className='text-sm font-semibold text-[#555] mx-0 my-2'
-                    >FraudTv</h3>
-                    <p className='text-sm' >15m views  &bull; 2 day ago</p>
-                </div>
-
-                <div className="card">
-                    <img className='w-full rounded-sm' src={thumbnail4} alt="" />
-                    <h2 className='font-semibold text-[16px] text-black mx-0 my-1.5'
-                    >How to earn Money from Youtube with in month just 30 Days.</h2>
-                    <h3 className='text-sm font-semibold text-[#555] mx-0 my-2'
-                    >FraudTv</h3>
-                    <p className='text-sm' >15m views  &bull; 2 day ago</p>
-                </div>
-
-                <div className="card">
-                    <img className='w-full rounded-sm' src={thumbnail5} alt="" />
-                    <h2 className='font-semibold text-[16px] text-black mx-0 my-1.5'
-                    >How to earn Money from Youtube with in month just 30 Days.</h2>
-                    <h3 className='text-sm font-semibold text-[#555] mx-0 my-2'
-                    >FraudTv</h3>
-                    <p className='text-sm' >15m views  &bull; 2 day ago</p>
-                </div>
-
-                <div className="card">
-                    <img className='w-full rounded-sm' src={thumbnail6} alt="" />
-                    <h2 className='font-semibold text-[16px] text-black mx-0 my-1.5'
-                    >How to earn Money from Youtube with in month just 30 Days.</h2>
-                    <h3 className='text-sm font-semibold text-[#555] mx-0 my-2'
-                    >FraudTv</h3>
-                    <p className='text-sm' >15m views  &bull; 2 day ago</p>
-                </div>
-
-                <div className="card">
-                    <img className='w-full rounded-sm' src={thumbnail7} alt="" />
-                    <h2 className='font-semibold text-[16px] text-black mx-0 my-1.5'
-                    >How to earn Money from Youtube with in month just 30 Days.</h2>
-                    <h3 className='text-sm font-semibold text-[#555] mx-0 my-2'
-                    >FraudTv</h3>
-                    <p className='text-sm' >15m views  &bull; 2 day ago</p>
-                </div>
-                <div className="card">
-                    <img className='w-full rounded-sm' src={thumbnail8} alt="" />
-                    <h2 className='font-semibold text-[16px] text-black mx-0 my-1.5'
-                    >How to earn Money from Youtube with in month just 30 Days.</h2>
-                    <h3 className='text-sm font-semibold text-[#555] mx-0 my-2'
-                    >FraudTv</h3>
-                    <p className='text-sm' >15m views  &bull; 2 day ago</p>
-                </div>
-
-                <div className="card">
-                    <img className='w-full rounded-sm' src={thumbnail3} alt="" />
-                    <h2 className='font-semibold text-[16px] text-black mx-0 my-1.5'
-                    >How to earn Money from Youtube with in month just 30 Days.</h2>
-                    <h3 className='text-sm font-semibold text-[#555] mx-0 my-2'
-                    >FraudTv</h3>
-                    <p className='text-sm' >15m views  &bull; 2 day ago</p>
-                </div>
-
-                <div className="card">
-                    <img className='w-full rounded-sm' src={thumbnail4} alt="" />
-                    <h2 className='font-semibold text-[16px] text-black mx-0 my-1.5'
-                    >How to earn Money from Youtube with in month just 30 Days.</h2>
-                    <h3 className='text-sm font-semibold text-[#555] mx-0 my-2'
-                    >FraudTv</h3>
-                    <p className='text-sm' >15m views  &bull; 2 day ago</p>
-                </div>
-
-                <div className="card">
-                    <img className='w-full rounded-sm' src={thumbnail4} alt="" />
-                    <h2 className='font-semibold text-[16px] text-black mx-0 my-1.5'
-                    >How to earn Money from Youtube with in month just 30 Days.</h2>
-                    <h3 className='text-sm font-semibold text-[#555] mx-0 my-2'
-                    >FraudTv</h3>
-                    <p className='text-sm' >15m views  &bull; 2 day ago</p>
-                </div>
-
-                <div className="card">
-                    <img className='w-full rounded-sm' src={thumbnail4} alt="" />
-                    <h2 className='font-semibold text-[16px] text-black mx-0 my-1.5'
-                    >How to earn Money from Youtube with in month just 30 Days.</h2>
-                    <h3 className='text-sm font-semibold text-[#555] mx-0 my-2'
-                    >FraudTv</h3>
-                    <p className='text-sm' >15m views  &bull; 2 day ago</p>
-                </div>
+            <div className="feed grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-4 gap-y-7 mt-4">
+                {data.map((items, index) => {
+                    return (
+                        <Link key={index} to={`video/${items.snippet.categoryId}/${items.id}`} className="card">
+                            <img className='w-full rounded-sm' src={items.snippet.thumbnails.high.url} alt="" />
+                            <h2 className='font-semibold text-[16px] text-black mx-0 my-1.5'
+                            >{items.snippet.title}</h2>
+                            <h3 className='text-sm font-semibold text-[#555] mx-0 my-2'
+                            >{items.snippet.channelTitle}</h3>
+                            <p className='text-sm'>{value_convertor(items.statistics.viewCount)} views &bull; {moment(items.snippet.publishedAt).fromNow()}</p>
+                        </Link>
+                    )
+                })}
             </div>
         </>
     )
 }
-
 
 export default Feed
